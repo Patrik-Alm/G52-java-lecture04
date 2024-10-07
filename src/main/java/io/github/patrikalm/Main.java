0192319fc22a0dc9ec5b63bf3355dd7c3ff8bcc2
Patrik Alm <almpatrik@yahoo.se>
2024-10-07 17:31:35 +0200

    Modified Main and completed excercise 8 using classes

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -3,8 +3,28 @@ package io.github.patrikalm;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println();
+        System.out.print("Guess a number between 0 and 500: ");
 
-      TestRandomSequence.testRandomSequence();
+        int counter = 0;
+        int theNumber = NumberGenerator.setRandomNumber();
+        int guessedNumber = GettingUserNumber.setGettingUserNumber();
+        System.out.println();
 
+        while (theNumber != guessedNumber) {
+
+            if (guessedNumber < theNumber) {
+                System.out.println("Your guess is too low");
+            } else {
+                System.out.println("Your guess too high");
+            }
+            System.out.println();
+            System.out.print("Guess again: ");
+            guessedNumber = GettingUserNumber.setGettingUserNumber();
+            counter++;
+            System.out.println();
+        }
+        System.out.println("You guessed the right number!!!");
+        System.out.println("It only took you " + counter + " guesses...");
     }
 }
