1be671d22e665c7c6252b25e72949be4c9d0e619
Patrik Alm <almpatrik@yahoo.se>
2024-10-07 16:07:48 +0200

    Added class GettingUserNumber

---
+++ src/main/java/io/github/patrikalm/GettingUserNumber.java
@@ -0,0 +1,14 @@
+package io.github.patrikalm;
+
+import java.util.Scanner;
+
+public class GettingUserNumber  {
+
+public static int setGettingUserNumber() {
+
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextInt();
+}
+
+
+}
