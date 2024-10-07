9d447c5d985864f8c86290d8d895fedeb0c268a1
Patrik Alm <almpatrik@yahoo.se>
2024-10-07 15:25:15 +0200

    Added class NumberGenerator

---
+++ src/main/java/io/github/patrikalm/NumberGenerator.java
@@ -0,0 +1,13 @@
+package io.github.patrikalm;
+
+public class NumberGenerator {
+
+
+    public static int setRandomNumber() {
+
+        int randomNumber = (int) Math.random() * 499 +1;
+
+        return randomNumber;
+    }
+
+}
